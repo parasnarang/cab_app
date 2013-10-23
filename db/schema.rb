4e59bc6342f419c80fa0644a9c5b3ca3e420272a
Paras Narang <narangparas@gmail.com>
2013-10-23 19:17:52 +0530

    DB migration for removing column no_of_passengers from cab_requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131023091157) do
+ActiveRecord::Schema.define(:version => 20131023134537) do
 
   create_table "admins", :force => true do |t|
     t.string  "name"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(:version => 20131023091157) do
     t.string   "requester"
     t.string   "traveler_name"
     t.string   "source"
-    t.integer  "no_of_passengers"
     t.datetime "pick_up_date_time"
     t.string   "status",            :default => "Pending"
     t.string   "vehicle_type"
