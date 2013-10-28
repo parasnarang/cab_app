59a7e5e6a7c73269df05c66e22f4b533b25f1ec4
Paras Narang <narangparas@gmail.com>
2013-10-28 12:09:40 +0530

    adding requester, admins to reply_to in email

--- app/mailers/cab_request_mailer.rb
+++ app/mailers/cab_request_mailer.rb
@@ -5,25 +5,25 @@ class CabRequestMailer < ActionMailer::Base
 
 # send a signup email to the user, pass in the user object that contains the user's email address
   def send_email(cab_request,pick_up_date,pick_up_time,requester,admin_emails,vendor_email)
-    mail(cc: [admin_emails,vendor_email,requester] , subject: "[Cab Request]", body:
+    mail(reply_to:[requester,admin_emails,vendor_email],  cc: [admin_emails,vendor_email,requester] , subject: "[Cab Request]", body:
 
-                                                        "Name            :\t" + cab_request.traveler_name+
+                                                        "Name :\t" + cab_request.traveler_name+
 
-                                                        "\nContact_no    :\t" + cab_request.contact_no+
+                                                        "\nContact_no :\t" + cab_request.contact_no+
 
-                                                        "\nFrom          :\t" + cab_request.source+
+                                                        "\nFrom :\t" + cab_request.source+
 
-                                                         "\nTo           :\t" + cab_request.destination+
+                                                        "\nTo :\t" + cab_request.destination+
 
-                                                         "\nOn           :\t" + pick_up_date+
+                                                        "\nOn :\t" + pick_up_date+
 
-                                                         "\nAt           :\t" + pick_up_time+
+                                                        "\nAt :\t" + pick_up_time+
 
-                                                         "\nVehicle Type :\t" + cab_request.vehicle_type+
+                                                        "\nVehicle Type :\t" + cab_request.vehicle_type+
 
-                                                         "\nComments     :\t" + cab_request.comments+
+                                                        "\nComments :\t" + cab_request.comments+
 
-                                                         "\nStatus       :\t" + cab_request.status)
+                                                        "\nStatus :\t" + cab_request.status)
 
   end
 end
